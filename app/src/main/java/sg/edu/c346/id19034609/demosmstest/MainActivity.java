8b998639100584e681fe63ab56554c61a298feef
Yee Hung <19034609@myrp.edy.sg>
2020-08-20 22:40:06 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/c346/id19034609/demosmstest/MainActivity.java
@@ -0,0 +1,47 @@
+package sg.edu.c346.id19034609.demosmstest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkPermission();
+
+        btnSend = findViewById(R.id.button);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage("5556",null,"Hello 5556",null,null);
+
+            }
+        });
+
+    }
+
+    private void checkPermission() {
+        int permissionSendSMS = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
+        int permissionRecvSMS = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+        if (permissionSendSMS != PackageManager.PERMISSION_GRANTED && permissionRecvSMS !=PackageManager.PERMISSION_GRANTED) {
+            String[] permissionNeeded = new  String[] {Manifest.permission.SEND_SMS,Manifest.permission.RECEIVE_SMS};
+            ActivityCompat.requestPermissions(this,permissionNeeded, 1);
+        }
+    }
+}
